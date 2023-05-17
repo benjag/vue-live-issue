9c3b34d327a984e87f5cfb8df2584d8660c0a51d
Ben Griffin <grif88@gmail.com>
2023-05-17 12:45:36 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { VueLive } from 'vue-live';
+</script>
+
+<template>
+  <main>
+    <VueLive
+      code="<h1>Hello world</h1>"
+      @error="(e) => console.log('Error on first example', e)"
+    />
+  </main>
+</template>
